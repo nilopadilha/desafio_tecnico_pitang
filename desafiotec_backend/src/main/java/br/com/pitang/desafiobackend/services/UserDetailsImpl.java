82ad0be979c7136cd4e6bf40243a7d37c8e58216
Nilopadilha <nilopadilha@gmail.com>
2024-11-01 15:28:35 -0300

    commit da classe UserDetailsImpl

---
+++ desafiotec_backend/src/main/java/br/com/pitang/desafiobackend/services/UserDetailsImpl.java
@@ -0,0 +1,63 @@
+package br.com.pitang.desafiobackend.services;
+
+import br.com.pitang.desafiobackend.enumerats.UserRole;
+import br.com.pitang.desafiobackend.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    private UserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRole.ADMIN)
+            return List.of(new SimpleGrantedAuthority("ADMIN"),
+                    new SimpleGrantedAuthority("GESTOR"),
+                    new SimpleGrantedAuthority("USER"));
+        else return List.of(new SimpleGrantedAuthority("USER"));
+    }
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    } // Retorna a credencial do usuário que criamos anteriormente
+
+    @Override
+    public String getUsername() {
+        return user.getLogin();
+    } // Retorna o nome de usuário do usuário que criamos anteriormente
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
+}
