876890cceef066e5244a284f6888c0d5fceb20e9
Nilopadilha <nilopadilha@gmail.com>
2024-11-01 15:30:14 -0300

    commit das classes de Serviço CarService e UserService

---
+++ desafiotec_backend/src/main/java/br/com/pitang/desafiobackend/services/CarService.java
@@ -0,0 +1,10 @@
+package br.com.pitang.desafiobackend.services;
+
+/**
+ * Representa interface de serviço com os métodos para carros no sistema.
+ */
+
+public interface CarService {
+
+
+}
---
+++ desafiotec_backend/src/main/java/br/com/pitang/desafiobackend/services/UserService.java
@@ -0,0 +1,10 @@
+package br.com.pitang.desafiobackend.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService  {
+
+
+
+}
