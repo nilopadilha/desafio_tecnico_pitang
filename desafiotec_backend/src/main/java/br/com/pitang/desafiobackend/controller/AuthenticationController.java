0d4ef3e1dde7c68dca22f2806871ac2eb844bd40
Nilopadilha <nilopadilha@gmail.com>
2024-11-01 15:34:29 -0300

    commit das classes de controller autenticaoa e usercontroller

---
+++ desafiotec_backend/src/main/java/br/com/pitang/desafiobackend/controller/AuthenticationController.java
@@ -0,0 +1,51 @@
+package br.com.pitang.desafiobackend.controller;
+
+import br.com.pitang.desafiobackend.dto.AutenticacaoDTO;
+import br.com.pitang.desafiobackend.dto.RegistrarUsuarioDTO;
+import br.com.pitang.desafiobackend.exceptions.NotFoundException;
+import br.com.pitang.desafiobackend.model.User;
+import br.com.pitang.desafiobackend.repositories.UserRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@Validated
+public class AuthenticationController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository usuarioRepository;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody @Valid AutenticacaoDTO usuarioLog) throws NotFoundException {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(usuarioLog.login(), usuarioLog.senha());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.status(HttpStatus.OK).body("logado com sucesso.");
+    }
+
+    @PostMapping("/registrar")
+    public ResponseEntity<?> registrar(@RequestBody @Valid RegistrarUsuarioDTO data) throws NotFoundException{
+        if(this.usuarioRepository.findByLogin(data.login()) != null) return  ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User newUsuario = new User(data.firstName(), data.lastName(), data.email(), data.birthday(), data.login(), encryptedPassword, data.phone(), data.role());
+
+        this.usuarioRepository.save(newUsuario);
+
+        return ResponseEntity.status(HttpStatus.OK).body("Usuario cadastrado com sucesso.");
+
+    }
+}
---
+++ desafiotec_backend/src/main/java/br/com/pitang/desafiobackend/controller/Usercontoller.java
@@ -0,0 +1,16 @@
+package br.com.pitang.desafiobackend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/users")
+
+public class Usercontoller {
+
+
+
+}
