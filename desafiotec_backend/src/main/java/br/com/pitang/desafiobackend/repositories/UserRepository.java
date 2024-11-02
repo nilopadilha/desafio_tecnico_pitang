2a68fb8f9e1981cd43211e6057dbcee4175c598a
Nilopadilha <nilopadilha@gmail.com>
2024-11-02 17:12:14 -0300

    criando metodos nas interface repository

--- desafiotec_backend/src/main/java/br/com/pitang/desafiobackend/repositories/UserRepository.java
+++ desafiotec_backend/src/main/java/br/com/pitang/desafiobackend/repositories/UserRepository.java
@@ -6,12 +6,24 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 /*
  * criação de classe repository da entidade Usuario.
  * */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String login);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByEmailAndIdNot(String email, Long id);
+
+    boolean existsByUsername(String login);
+
+    boolean existsByUsernameAndIdNot(String login, Long id);
 
     UserDetails findByLogin(String login);
 }
