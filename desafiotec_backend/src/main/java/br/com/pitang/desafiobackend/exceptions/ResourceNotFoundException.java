5ce26c30c8916c9a885193e862fdc8857f993663
Nilopadilha <nilopadilha@gmail.com>
2024-11-11 12:29:02 -0300

    alterações realizadas na classe exception

--- desafiotec_backend/src/main/java/br/com/pitang/desafiobackend/exceptions/ResourceNotFoundException.java
+++ desafiotec_backend/src/main/java/br/com/pitang/desafiobackend/exceptions/ResourceNotFoundException.java
@@ -1,11 +1,27 @@
 package br.com.pitang.desafiobackend.exceptions;
 
 /*
-* classe que trata os erros do sistemas
-* */
+ * classe que trata os erros do sistemas
+ * */
 
-public class ResourceNotFoundException extends RuntimeException{
+public class ResourceNotFoundException extends RuntimeException {
+
     public ResourceNotFoundException(String message) {
         super(message);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ResourceNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public ResourceNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public ResourceNotFoundException() {
     }
 }
